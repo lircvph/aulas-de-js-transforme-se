6b21a3ee074c6359d0641c405a034869cb4135a0
Sophia Liz <cavalcantesophializ@gmail.com>
2026-02-24 21:59:28 -0300

    first commit

---
+++ Aula1/SRC/script.js
@@ -0,0 +1,12 @@
+console.log("Testando JavaScript");
+
+let nome = "Liz";
+let nome2 = "Nathalia";
+
+console.log(nome);
+console.log(nome2);
+//ouuuu
+console.log(nome, nome2);
+
+const times = ["Corinthians", "São Paulo", "Santos"];
+console.log(times);
