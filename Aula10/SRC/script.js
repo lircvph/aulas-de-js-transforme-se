d72931ee49b540af7858fc165f8de8aeb2689b6e
Sophia Liz <cavalcantesophializ@gmail.com>
2026-03-10 21:22:57 -0300

    Aula de 10/03/26 - Finalização de Funções

---
+++ Aula10/SRC/script.js
@@ -0,0 +1,19 @@
+//Definição padrão de função
+
+function soma(a, b){
+    return (a + b);
+}
+
+let a = soma(30, 70);
+console.log(a);
+
+// Função armazenada em variável
+const multiplicar = function (a, b) {return (a * b)};
+I
+
+let b = multiplicar(3, 4);
+console.log(b);
+
+//Arrow Function 
+let subtracao = (a, b) => (a - b);
+let = subtracao 
